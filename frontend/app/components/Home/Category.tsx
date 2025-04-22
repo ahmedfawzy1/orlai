7738edd43bd83092cea530d2da99f1b955555dfe
ahmedfawzy1 <ahmedfawzy100038@gmail.com>
2025-04-22 12:20:49 +0200

    refactor: enhance category component

--- frontend/app/components/Home/Category.tsx
+++ frontend/app/components/Home/Category.tsx
@@ -78,50 +78,54 @@ export default function Category() {
           Shop by Categories
         </h5>
 
-        <div className='flex flex-nowrap flex-row pt-1'>
+        <div className='flex flex-nowrap flex-row gap-4 pt-1'>
           <button
             onClick={() => scroll('left')}
             aria-label='Scroll left'
             disabled={!scrollLeft}
-            className={`p-2 rounded-full ${
-              scrollLeft ? 'hover:bg-gray-100' : ''
+            className={`p-2.5 bg-[#f3f3f3] rounded-[8px] ${
+              scrollLeft ? 'bg-black' : ''
             } transition-colors`}
           >
-            <ArrowLeft color={scrollLeft ? '#000' : '#ccc'} />
+            <ArrowLeft size={16} color={scrollLeft ? '#fff' : '#ccc'} />
           </button>
           <button
             onClick={() => scroll('right')}
             aria-label='Scroll right'
             disabled={!scrollRight}
-            className={`p-2 rounded-full ${
-              scrollRight ? 'hover:bg-gray-100' : ''
+            className={`p-2.5 bg-[#f3f3f3] rounded-[8px] ${
+              scrollRight ? 'bg-black' : ''
             } transition-colors`}
           >
-            <ArrowRight color={scrollRight ? '#000' : '#ccc'} />
+            <ArrowRight size={16} color={scrollRight ? '#fff' : '#ccc'} />
           </button>
         </div>
       </div>
 
       <div
         ref={scrollRef}
-        className='w-full mt-6 md:mt-10 flex gap-4 md:gap-8 overflow-x-auto scrollbar-hide snap-x snap-mandatory'
+        className='w-full mt-6 md:mt-7 flex gap-4 md:gap-8 overflow-x-auto scrollbar-hide snap-x snap-mandatory'
       >
         {categories.map(category => (
           <div
             key={category.id}
-            className='bg-[#f3f3f3] w-[280px] md:w-[calc(33.333%-1.5rem)] lg:w-[calc(25%-2rem)] h-[250px] md:h-[300px] relative flex-shrink-0 snap-start'
+            className='bg-[#f3f3f3] w-[280px] md:w-[calc(33.333%-1.5rem)] lg:w-[calc(25%-2rem)] h-[250px] md:h-[300px] relative flex-shrink-0 overflow-hidden'
           >
+            <h3 className='text-6xl font-bold text-[#e8e8e8] absolute top-2 left-6'>
+              {category.name.split(' ')[0]}
+            </h3>
             <Image
               src={category.image}
               alt={category.name}
               width={488}
               height={843}
-              className='w-full h-full object-contain'
+              className='w-full h-full object-contain relative z-10'
               draggable={false}
+              priority={true}
             />
             <Link
-              href={'/'}
-              className='w-[calc(100%-2rem)] absolute bottom-5 left-1/2 -translate-x-1/2 bg-white rounded-xl px-4 py-2 text-black text-lg md:text-xl font-bold text-center'
+              href={`/shop?category=${category.name}`}
+              className='px-4 py-2 text-black text-lg font-medium text-center w-[calc(100%-2rem)] absolute bottom-5 left-1/2 z-10 -translate-x-1/2 bg-white rounded-lg hover:scale-105 transition-all duration-300'
             >
               {category.name}
             </Link>
