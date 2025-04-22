63aa68157c9be969abd081d60291dc976ece2bda
ahmedfawzy1 <ahmedfawzy100038@gmail.com>
2025-04-22 15:13:59 +0200

    refactor: improve structure

---
+++ frontend/app/types/product.ts
@@ -0,0 +1,18 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  style: string;
+  store: string;
+  size: string;
+  inventory: number;
+  color: string;
+  minPrice: number;
+  maxPrice: number;
+  image: string;
+  userId: number;
+  slug: string;
+  averageRating: number;
+  createdAt: string;
+}
