9ff8fd7ecda0ed20aab9cb3177d1dc3e05a80bde
ahmedfawzy1 <ahmedfawzy100038@gmail.com>
2025-04-25 17:14:36 +0300

    fix: reload page on nanbar

--- frontend/app/components/Navbar/DesktopNav.tsx
+++ frontend/app/components/Navbar/DesktopNav.tsx
@@ -38,7 +38,22 @@ const renderMenuItem = (item: MenuItem) => {
         <NavigationMenuContent className='bg-popover text-popover-foreground'>
           {item.items.map(subItem => (
             <NavigationMenuLink asChild key={subItem.title} className='w-80'>
-              <SubMenuLink item={subItem} />
+              <Link
+                href={subItem.url}
+                className='flex flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none hover:bg-muted hover:text-accent-foreground'
+              >
+                <div className='text-foreground'>
+                  {subItem.icon && icons[subItem.icon]}
+                </div>
+                <div>
+                  <div className='text-sm font-semibold'>{subItem.title}</div>
+                  {subItem.description && (
+                    <p className='text-sm leading-snug text-muted-foreground'>
+                      {subItem.description}
+                    </p>
+                  )}
+                </div>
+              </Link>
             </NavigationMenuLink>
           ))}
         </NavigationMenuContent>
@@ -48,32 +63,13 @@ const renderMenuItem = (item: MenuItem) => {
 
   return (
     <NavigationMenuItem key={item.title}>
-      <NavigationMenuLink
+      <Link
         href={item.url}
         className='group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground'
       >
         {item.title}
-      </NavigationMenuLink>
+      </Link>
     </NavigationMenuItem>
-  );
-};
-
-const SubMenuLink = ({ item }: { item: MenuItem }) => {
-  return (
-    <Link
-      className='flex flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none hover:bg-muted hover:text-accent-foreground'
-      href={item.url}
-    >
-      <div className='text-foreground'>{item.icon && icons[item.icon]}</div>
-      <div>
-        <div className='text-sm font-semibold'>{item.title}</div>
-        {item.description && (
-          <p className='text-sm leading-snug text-muted-foreground'>
-            {item.description}
-          </p>
-        )}
-      </div>
-    </Link>
   );
 };
 
