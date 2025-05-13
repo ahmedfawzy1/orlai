2d176ac5ad36dadd6d0bd42f10b5f33c22c1901f
ahmedfawzy1 <ahmedfawzy100038@gmail.com>
2025-05-14 02:38:21 +0300

    feat: linking products to categories

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -19,9 +19,9 @@ const productSchema = new mongoose.Schema(
       },
     ],
     category: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
       required: true,
-      trim: true,
     },
     availableForSale: {
       type: Boolean,
